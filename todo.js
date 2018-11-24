bee417c1e0b680412ace72ac2671a281e9dbdd63
KIMINJOONG <ijboym216@gmail.com>
2018-11-24 23:21:05 +0900

    update function to change background and delete toDos

--- todo.js
+++ todo.js
@@ -6,7 +6,22 @@ const todoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+
+let toDos = [];
+
+
+
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
@@ -16,6 +31,7 @@ function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "X";
+    delBtn.addEventListener("click",  deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text;
